92cd722dbbb0d9ea7415fa3fa1b2e3f907cb4a03
Jillian Rose Formeloza <jillianroseformeloza@gmail.com>
2026-03-02 15:49:14 +0800

    add all the necessary files in my github

--- croptails/scripts/state_machine/node_state_machine.gd
+++ croptails/scripts/state_machine/node_state_machine.gd
@@ -16,6 +16,7 @@ func _ready() -> void:
 	if initial_node_state:
 		initial_node_state._on_enter()
 		current_node_state = initial_node_state
+		current_node_state_name = current_node_state.name.to_lower()
 
 
 func _process(delta : float) -> void:
@@ -27,7 +28,7 @@ func _physics_process(delta: float) -> void:
 	if current_node_state:
 		current_node_state._on_physics_process(delta)
 		current_node_state._on_next_transitions()
-		#print("Current State: ", current_node_state_name)
+		print("Current State: ", current_node_state_name)
 
 
 func transition_to(node_state_name : String) -> void:
@@ -46,4 +47,4 @@ func transition_to(node_state_name : String) -> void:
 	
 	current_node_state = new_node_state
 	current_node_state_name = current_node_state.name.to_lower()
-	#print("Current State: ", current_node_state_name)
+	print("Current State: ", current_node_state_name)
